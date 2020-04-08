52315dd6a8aef081816e6fd8e291c2e8b8eb4fc8
camckenzie <63214003+camckenzie@users.noreply.github.com>
2020-04-08 02:10:35 -0400

    HW10 Test

--- Student_Repository_Test_Christopher_McKenzie.py
+++ Student_Repository_Test_Christopher_McKenzie.py
@@ -1,7 +1,6 @@
 """Christopher McKenzie
 Test that the Repository class accurately prints the information
-required for the Student Pretty Table and the Instrucor Pretty
-Table.
+required for the Major, Student, and Instructor Pretty Tables.
 """
 
 import unittest
@@ -10,16 +9,28 @@ from collections import defaultdict
 import os
 from prettytable import PrettyTable
 from HW08_Christopher_McKenzie import file_reader
-from Student_Repository_Christopher_McKenzie import Student, Instructor, Repository
+from Student_Repository_Christopher_McKenzie import Student, Instructor, Major, Repository
 
 
 class RepositoryTest(unittest.TestCase):
 
 
-    """Verify that Repositroy class prints Student and Instructor
-    info correctly.
+    """Verify that Repositroy class prints Major, Student and
+    Instructor info correctly.
     """
 
+    def test_majors(self) -> None:
+
+        """Verify that major information that goes into pretty
+        table is set up correctly.
+        """
+
+        a = Repository(r"C:\Users\Chris\Desktop\HW10", False)   
+        prd = {major: courses.info() for major, courses in a._majors.items()}
+        exp = {'SFEN': ['SFEN', ['SSW 540', 'SSW 564', 'SSW 555', 'SSW 567'],['CS 501', 'CS 513', 'CS 545']],
+                'SYEN': ['SYEN', ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540']]}
+        
+        self.assertEqual(prd, exp)
 
     def test_students(self) -> None:
 
@@ -27,18 +38,18 @@ class RepositoryTest(unittest.TestCase):
         table is set up correctly.
         """
 
-        a = Repository(r"C:\Users\Chris\Desktop\HW09", False)   
-        prd = {cwid: student.info() for cwid, student in a._students.items()}
-        exp = {'10103': ['10103', 'Baldwin, C', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687']],
-                    '10115': ['10115', 'Wyatt, X', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687']],
-                    '10172': ['10172', 'Forbes, I', ['SSW 555', 'SSW 567']],
-                    '10175': ['10175', 'Erickson, D', ['SSW 564', 'SSW 567', 'SSW 687']],
-                    '10183': ['10183', 'Chapman, O', ['SSW 689']],
-                    '11399': ['11399', 'Cordova, I', ['SSW 540']],
-                    '11461': ['11461', 'Wright, U', ['SYS 611', 'SYS 750', 'SYS 800']],
-                    '11658': ['11658', 'Kelly, P', ['SSW 540']],
-                    '11714': ['11714', 'Morton, A', ['SYS 611', 'SYS 645']],
-                    '11788': ['11788', 'Fuller, E', ['SSW 540']]}
+        b = Repository(r"C:\Users\Chris\Desktop\HW10", False)   
+        prd = {cwid: student.info() for cwid, student in b._students.items()}
+        exp = {'10103': ['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
+                '10115': ['10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81],
+                '10172': ['10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88],
+                '10175': ['10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58],
+                '10183': ['10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 564', 'SSW 555', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 4.0],
+                '11399': ['11399', 'Cordova, I', 'SYEN', ['SSW 540'], ['SYS 671', 'SYS 612', 'SYS 800'], [], 3.0],
+                '11461': ['11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 671', 'SYS 612'], ['SSW 810', 'SSW 565', 'SSW 540'], 3.92],
+                '11658': ['11658', 'Kelly, P', 'SYEN', [], ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540'], 0],
+                '11714': ['11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540'], 3.0],
+                '11788': ['11788', 'Fuller, E', 'SYEN', ['SSW 540'], ['SYS 671', 'SYS 612', 'SYS 800'], [], 4.0]}
         self.assertEqual(prd, exp)
 
     def test_instructors(self) -> None:
@@ -47,8 +58,8 @@ class RepositoryTest(unittest.TestCase):
         table is set up correctly.
         """
 
-        b = Repository(r"C:\Users\Chris\Desktop\HW09", False)
-        prd = {tuple(info) for instructor in b._instructors.values() for info in instructor.info()}
+        c = Repository(r"C:\Users\Chris\Desktop\HW10", False)
+        prd = {tuple(info) for instructor in c._instructors.values() for info in instructor.info()}
         exp = {('98760', 'Darwin, C', 'SYEN', 'SYS 800', 1),
                     ('98760', 'Darwin, C', 'SYEN', 'SYS 611', 2),
                     ('98760', 'Darwin, C', 'SYEN', 'SYS 645', 1),
