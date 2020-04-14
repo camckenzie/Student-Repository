c4dcbe0c13c820038796556818053d0c2686aab8
camckenzie <63214003+camckenzie@users.noreply.github.com>
2020-04-14 19:18:00 -0400

    HW11 Tests
    Included test for Student Grades pretty table

--- Student_Repository_Test_Christopher_McKenzie.py
+++ Student_Repository_Test_Christopher_McKenzie.py
@@ -6,7 +6,7 @@ required for the Major, Student, and Instructor Pretty Tables.
 import unittest
 from typing import DefaultDict, Tuple, Iterator, List, Dict, IO
 from collections import defaultdict
-import os
+import os, sqlite3
 from prettytable import PrettyTable
 from HW08_Christopher_McKenzie import file_reader
 from Student_Repository_Christopher_McKenzie import Student, Instructor, Major, Repository
@@ -25,10 +25,11 @@ class RepositoryTest(unittest.TestCase):
         table is set up correctly.
         """
 
-        a = Repository(r"C:\Users\Chris\Desktop\HW10", False)   
+        a = Repository(r"C:\Users\Chris\Desktop\HW11", False)   
         prd = {major: courses.info() for major, courses in a._majors.items()}
-        exp = {'SFEN': ['SFEN', ['SSW 540', 'SSW 564', 'SSW 555', 'SSW 567'],['CS 501', 'CS 513', 'CS 545']],
-                'SYEN': ['SYEN', ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540']]}
+        exp = {'SFEN': ['SFEN', ['SSW 540', 'SSW 810', 'SSW 555'], ['CS 501', 'CS 546']],
+            'CS': ['CS', ['CS 570', 'CS 546'], ['SSW 810', 'SSW 565']]
+            }
         
         self.assertEqual(prd, exp)
 
@@ -38,41 +39,56 @@ class RepositoryTest(unittest.TestCase):
         table is set up correctly.
         """
 
-        b = Repository(r"C:\Users\Chris\Desktop\HW10", False)   
+        b = Repository(r"C:\Users\Chris\Desktop\HW11", False)   
         prd = {cwid: student.info() for cwid, student in b._students.items()}
-        exp = {'10103': ['10103', 'Baldwin, C', 'SFEN', ['CS 501', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.44],
-                '10115': ['10115', 'Wyatt, X', 'SFEN', ['CS 545', 'SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], [], 3.81],
-                '10172': ['10172', 'Forbes, I', 'SFEN', ['SSW 555', 'SSW 567'], ['SSW 540', 'SSW 564'], ['CS 501', 'CS 513', 'CS 545'], 3.88],
-                '10175': ['10175', 'Erickson, D', 'SFEN', ['SSW 564', 'SSW 567', 'SSW 687'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 513', 'CS 545'], 3.58],
-                '10183': ['10183', 'Chapman, O', 'SFEN', ['SSW 689'], ['SSW 540', 'SSW 564', 'SSW 555', 'SSW 567'], ['CS 501', 'CS 513', 'CS 545'], 4.0],
-                '11399': ['11399', 'Cordova, I', 'SYEN', ['SSW 540'], ['SYS 671', 'SYS 612', 'SYS 800'], [], 3.0],
-                '11461': ['11461', 'Wright, U', 'SYEN', ['SYS 611', 'SYS 750', 'SYS 800'], ['SYS 671', 'SYS 612'], ['SSW 810', 'SSW 565', 'SSW 540'], 3.92],
-                '11658': ['11658', 'Kelly, P', 'SYEN', [], ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540'], 0],
-                '11714': ['11714', 'Morton, A', 'SYEN', ['SYS 611', 'SYS 645'], ['SYS 671', 'SYS 612', 'SYS 800'], ['SSW 810', 'SSW 565', 'SSW 540'], 3.0],
-                '11788': ['11788', 'Fuller, E', 'SYEN', ['SSW 540'], ['SYS 671', 'SYS 612', 'SYS 800'], [], 4.0]}
+        exp = {'10103': ['10103', 'Jobs, S', 'SFEN', ['CS 501', 'SSW 810'], ['SSW 540', 'SSW 555'], [], 3.38],
+            '10115': ['10115', 'Bezos, J', 'SFEN', ['SSW 810'], ['SSW 540', 'SSW 555'], ['CS 501', 'CS 546'], 4.0],
+            '10183': ['10183', 'Musk, E', 'SFEN', ['SSW 555', 'SSW 810'], ['SSW 540'], ['CS 501', 'CS 546'], 4.0],
+            '11714': ['11714', 'Gates, B', 'CS', ['CS 546', 'CS 570', 'SSW 810'], [], [], 3.5]}
         self.assertEqual(prd, exp)
 
     def test_instructors(self) -> None:
 
-        """Verify that student information that goes into pretty
+        """Verify that instructor information that goes into pretty
         table is set up correctly.
         """
 
-        c = Repository(r"C:\Users\Chris\Desktop\HW10", False)
+        c = Repository(r"C:\Users\Chris\Desktop\HW11", False)
         prd = {tuple(info) for instructor in c._instructors.values() for info in instructor.info()}
-        exp = {('98760', 'Darwin, C', 'SYEN', 'SYS 800', 1),
-                    ('98760', 'Darwin, C', 'SYEN', 'SYS 611', 2),
-                    ('98760', 'Darwin, C', 'SYEN', 'SYS 645', 1),
-                    ('98763', 'Newton, I', 'SFEN', 'SSW 689', 1),
-                    ('98760', 'Darwin, C', 'SYEN', 'SYS 750', 1),
-                    ('98765', 'Einstein, A', 'SFEN', 'SSW 567', 4),
-                    ('98763', 'Newton, I', 'SFEN', 'SSW 555', 1),
-                    ('98764', 'Feynman, R', 'SFEN', 'CS 545', 1),
-                    ('98765', 'Einstein, A', 'SFEN', 'SSW 540', 3),
-                    ('98764', 'Feynman, R', 'SFEN', 'CS 501', 1),
-                    ('98764', 'Feynman, R', 'SFEN', 'SSW 687', 3),
-                    ('98764', 'Feynman, R', 'SFEN', 'SSW 564', 3)}
+        exp = {('98763', 'Rowland, J', 'SFEN', 'SSW 555', 1),
+                ('98762', 'Hawking, S', 'CS', 'CS 546', 1),
+                ('98764', 'Cohen, R', 'SFEN', 'CS 546', 1),
+                ('98763', 'Rowland, J', 'SFEN', 'SSW 810', 4),
+                ('98762', 'Hawking, S', 'CS', 'CS 501', 1),
+                ('98762', 'Hawking, S', 'CS', 'CS 570', 1)}
         self.assertEqual(prd, exp)
+
+    def test_student_grades(self) -> None:
+
+        """Verify that student grade information that goes into
+        pretty table is set up correctly.
+        """
+        d = Repository(r"C:\Users\Chris\Desktop\HW11", False)
+        
+        DB_FILE: str = r"C:\Users\Chris\Desktop\HW11\HW11_Database.db"
+        db: sqlite3.Connection = sqlite3.connect(DB_FILE)
+        query: str = """select s.Name as Student, s.CWID, g.Course, g.Grade, i.Name as Instructor
+                from students s
+                    join grades g on s.CWID=g.StudentCWID
+                    join instructors i on i.CWID=g.InstructorCWID
+                    order by Student"""
+                    
+        prd = {row for row in db.execute(query)}
+        exp = {('Musk, E', '10183', 'SSW 810', 'A', 'Rowland, J'),
+                ('Gates, B', '11714', 'SSW 810', 'B-', 'Rowland, J'),
+                ('Jobs, S', '10103', 'CS 501', 'B', 'Hawking, S'),
+                ('Jobs, S', '10103', 'SSW 810', 'A-', 'Rowland, J'),
+                ('Bezos, J', '10115', 'SSW 810', 'A', 'Rowland, J'),
+                ('Gates, B', '11714', 'CS 546', 'A', 'Cohen, R'),
+                ('Bezos, J', '10115', 'CS 546', 'F', 'Hawking, S'),
+                ('Musk, E', '10183', 'SSW 555', 'A', 'Rowland, J'),
+                ('Gates, B', '11714', 'CS 570', 'A-', 'Hawking, S')
+                }
 
 if __name__ == '__main__':
     unittest.main(exit=False, verbosity=2)
